b78f223191f181b23958d123af8a49a28e3be5fc
Alfred <kanalfred@gmail.com>
2016-03-22 17:35:42 -0700

    Update default async

--- src/ServiceClient/Openvpn.php
+++ src/ServiceClient/Openvpn.php
@@ -7,9 +7,8 @@ use Wvpn\Client\Exception as Exception;
 class Openvpn extends Base
 {
    /**
-    * Openvpn 
+    * Client Config
     */
-   // Client Config
    public function getClientConfig($account, $server, $downloadable = false, $filename = 'wpvn.opvn',  $async = false)
    {
         $data = ['account' => $account, 'server' => $server];
@@ -30,45 +29,52 @@ class Openvpn extends Base
         }
    }
 
-   public function getClientParam($account)
+   public function getClientParam($account, $async=false)
    {
         $data = ['account' => $account];
         $options = ['json' => $data];
+        $options['future'] = $async;
 
         return $response = $this->post( 'getClientParam', $options );
    }
 
-   public function setClientParam($account, $key, $value)
+   public function setClientParam($account, $key, $value, $async=false)
    {
         $data = ['account' => $account];
         $data = ['key' => $key];
         $data = ['value' => $value];
         $options = ['json' => $data];
+        $options['future'] = $async;
         
         return $response = $this->post( 'setClientParam', $options );
    }
 
-   public function setClientParams($account, array $data)
+   public function setClientParams($account, array $data, $async=false)
    {
         $options = ['json' => $data];
+        $options['future'] = $async;
         
         return $response = $this->post( 'setClientParams', $options );
    }
 
-   public function deleteClientParam($account, $key, $value)
+   public function deleteClientParam($account, $key, $value, $async=false)
    {
         $data = ['account' => $account];
         $data = ['key' => $key];
         $options = ['json' => $data];
+        $options['future'] = $async;
 
         return $response = $this->post( 'deleteClientParam', $options );
    }
 
-   // Server Status
-   public function getServerStatus($host = null)
+   /**
+    * Server Status 
+    */
+   public function getServerStatus($host = null, $async=false)
    {
         $data = ['host' => $host];
         $options = ['json' => $data];
+        $options['future'] = $async;
         return $this->post( 'getServerStatus', $options );
    }
 
@@ -76,6 +82,7 @@ class Openvpn extends Base
    {
         $data = ['async' => $async];
         $options = ['json' => $data];
+        $options['future'] = $async;
         return $this->post( 'fetchServerStatusJob', $options);
    }
 
